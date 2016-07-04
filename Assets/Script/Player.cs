90b76eef8ee56bc54cbeb0fb28433f9b38cd4bfb
Dan Tran <dantrandinhh@gmail.com>
2016-07-04 21:43:17 +0700

    Move

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour
+{
+
+	public float speed;
+	public float moveX;
+
+	Rigidbody2D rigid;
+	Animator anim;
+	Vector3 localscale;
+
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+
+		localscale = transform.localScale;
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		moveX = Input.GetAxis ("Horizontal");
+
+//		if (moveX > 0) {
+//			//facing right
+//			localscale.x = 1;
+//			transform.localScale = localscale;
+//			anim.SetTrigger ("run");
+//		}
+//
+//
+//		if (moveX < 0) {
+//			//facing left
+//			localscale.x = -1;
+//			transform.localScale = localscale;
+//			anim.SetTrigger ("run");
+//		} s
+	
+
+		rigid.velocity = new Vector2 (moveX * speed, 0);
+
+	}
+
+	#region combo attack
+
+	int comboCount = 0;
+	//đếm chỉ số combo hiện tại
+	bool isCombo;
+	//biến bool xác định player có thực hiện combo attack hay chỉ đánh đơn
+	bool isNext = true;
+	//biến bool xác định player có đánh tiếp hay ko
+
+	public void Attack ()
+	{
+		//Debug.Log("combo count = " + comboCount);
+		comboCount++;
+		if (comboCount > 3) {
+			comboCount = 1;
+		}
+		//Debug.Log("combo : " + comboCount);
+
+		switch (comboCount) {
+		case 1:
+			StopCoroutine ("SetComboTrue");
+			StopCoroutine ("SetComboFalse");
+			isNext = false;
+			anim.Play ("Atk1");
+				//Debug.Log ("Combo 1");
+			StartCoroutine ("SetComboFalse");
+			break;
+
+		case 2:
+			StopCoroutine ("SetComboTrue");
+			StopCoroutine ("SetComboFalse");
+			isNext = false;
+			anim.Play ("Atk2");
+				//Debug.Log ("Combo 2");
+			StartCoroutine ("SetComboFalse");
+			break;
+
+		case 3:
+			StopCoroutine ("SetComboTrue");
+			StopCoroutine ("SetComboFalse");
+			isNext = false;
+			anim.Play ("Atk3");
+				//Debug.Log ("Combo 3");
+			StartCoroutine ("SetComboFalse");
+			break;
+
+		default:
+			break;
+		}
+		StopCoroutine ("ResetComboCount");
+
+		StartCoroutine ("ResetComboCount");
+	}
+
+	
+	IEnumerator SetComboTrue ()
+	{
+		yield return new WaitForSeconds (1f);
+		isCombo = true;
+	}
+
+	IEnumerator SetComboFalse ()
+	{
+		yield return new WaitForSeconds (0.4f);
+		isNext = true;
+		isCombo = false;
+	}
+
+	IEnumerator ResetComboCount ()
+	{
+		//if (isCombo == false)
+		//{
+		yield return new WaitForSeconds (0.6f);
+		comboCount = 0;
+		isNext = true;
+		anim.Play ("Idle");
+		//Debug.Log("Reset Combo count!!");
+		//}
+	}
+
+	#endregion
+
+
+
+}
---
+++ Assets/VirtualJoystick.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+	private Image backgroundImage;
+	private Image joystickImage;
+
+	private Vector3 inputVector;
+
+	void Start ()
+	{
+		backgroundImage = GetComponent<Image> ();
+		joystickImage = transform.GetChild (0).GetComponent<Image> ();
+	}
+
+	public virtual void OnDrag (PointerEventData eventData)
+	{
+		Vector2 pos;
+		if (RectTransformUtility.
+			ScreenPointToLocalPointInRectangle (
+			    backgroundImage.rectTransform,
+			    eventData.position, 
+			    eventData.pressEventCamera,  
+			    out pos)) {
+			
+			pos.x = (pos.x / backgroundImage.rectTransform.sizeDelta.x);
+			pos.y = (pos.y / backgroundImage.rectTransform.sizeDelta.y);
+
+			inputVector = new Vector3 (pos.x * 2 + 1, 0, pos.y * 2 - 1);
+			inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+			//move joystick image
+			joystickImage.rectTransform.anchoredPosition =
+				new Vector3 (inputVector.x * (backgroundImage.rectTransform.sizeDelta.x / 3),
+				inputVector.z * (backgroundImage.rectTransform.sizeDelta.y / 3));
+							
+		}
+				
+	}
+
+	public virtual void OnPointerUp (PointerEventData eventData)
+	{
+		inputVector = Vector3.zero;
+		joystickImage.rectTransform.anchoredPosition = Vector3.zero;
+	}
+
+	public virtual void OnPointerDown (PointerEventData eventData)
+	{
+		OnDrag (eventData);	
+	}
+}
